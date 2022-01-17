3c40f196248ede3fe56841db7a708379d879b687
João Paulo <lobaojoao2@gmail.com>
2022-01-16 23:19:59 -0300

    ajusta nome da variável

--- teste.py
+++ teste.py
@@ -156,9 +156,9 @@ salario = 4000
 despesas = 3900
 
 saldo_positivo = saldo > 0
-despesas_controlasdas = salario - despesas >= 0.2 * salario
+despesas_controladas = salario - despesas >= 0.2 * salario
 
-meta = saldo_positivo and despesas_controlasdas
+meta = saldo_positivo and despesas_controladas
 meta
 
 # Desafio Operados Lógicos
