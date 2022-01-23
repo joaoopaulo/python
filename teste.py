949e31b04a218af37b3b7311ff1237fc901a7a15
João Paulo <lobaojoao2@gmail.com>
2022-01-23 02:05:30 -0300

    confunto fundamentos

--- teste.py
+++ teste.py
@@ -1,4 +1,5 @@
 # Primeiros Exemplos
+from calendar import c
 import decimal
 from decimal import Decimal, getcontext
 from re import T, X
@@ -510,3 +511,28 @@ del pessoa['cursos']
 pessoa
 pessoa.clear()
 pessoa
+
+
+# Conjunto
+a = {1, 2, 3}
+type(a)
+# a[0]
+a = set('coddddd3r')
+print(a)
+print('3' in a, 4 not in a)
+{1, 2, 3} == {3, 2, 1, 3}
+
+# operações
+c1 = {1, 2}
+c2 = {2, 3}
+c1.union(c2)
+c1.intersection(c2)
+c1.update(c2)
+c1
+c2 <= c1
+c1 >= c2
+
+{1, 2, 3} - {2}
+c1 - c2
+c1 -= {2}
+c1
